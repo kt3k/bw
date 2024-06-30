33cdfc59047f178831e2a677e3c427e0677b537f
Yoshiya Hinosawa <stibium121@gmail.com>
2024-06-30 21:48:59 +0900

    feat: draw image

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,23 @@
 import { type Context, register } from "@kt3k/cell"
 import { gameloop } from "@kt3k/gameloop"
 
+function loadImage(path: string): Promise<Image> {
+  return new Promise((resolve, reject) => {
+    const img = new Image()
+    img.onload = () => {
+      resolve(img)
+    }
+    img.onerror = (e) => {
+      reject(e)
+    }
+    img.src = path
+  })
+}
+
+function loadImages(paths: string[]): Promise<Image[]> {
+  return Promise.all(paths.map(loadImage))
+}
+
 function Canvas1({ el, pub }: Context<HTMLCanvasElement>) {
   const WIDTH = +el.width
   const HEIGHT = +el.height
@@ -10,23 +27,39 @@ function Canvas1({ el, pub }: Context<HTMLCanvasElement>) {
 
   const canvasCtx = el.getContext("2d")!
 
+  /*
   for (const i of Array(W).keys()) {
     for (const j of Array(H).keys()) {
       canvasCtx.fillStyle = (i + j) % 2 === 0 ? "black" : "gray"
       canvasCtx.fillRect(i * 16, j * 16, 16, 16)
     }
   }
+    */
 
-  const loop = gameloop(() => {
-    for (const _ of Array(W).keys()) {
+  loadImages([
+    "./src/images/char/juni/juni_b0.png",
+    "./src/images/char/juni/juni_b1.png",
+    "./src/images/char/juni/juni_f0.png",
+    "./src/images/char/juni/juni_f1.png",
+    "./src/images/char/juni/juni_l0.png",
+    "./src/images/char/juni/juni_l1.png",
+    "./src/images/char/juni/juni_r0.png",
+    "./src/images/char/juni/juni_r1.png",
+  ]).then((images) => {
+    console.log(images)
+    const loop = gameloop(() => {
       const i = Math.floor(Math.random() * W)
       const j = Math.floor(Math.random() * H)
-      canvasCtx.fillStyle = Math.random() > 0.5 ? "black" : "gray"
-      canvasCtx.fillRect(i * 16, j * 16, 16, 16)
-    }
-  }, 60)
-  loop.onStep((fps) => pub("fps", fps))
-  loop.run()
+      canvasCtx.clearRect(i * 16, j * 16, 16, 16)
+      canvasCtx.drawImage(
+        images[Math.floor(Math.random() * 8)],
+        i * 16,
+        j * 16,
+      )
+    }, 60)
+    loop.onStep((fps) => pub("fps", fps))
+    loop.run()
+  })
 }
 
 function Canvas2({ el }: Context<HTMLCanvasElement>) {
@@ -45,7 +78,6 @@ function FPSMonitor({ sub, on, el }: Context) {
   return "0"
 }
 
-register(Hello, "hello")
 register(Canvas1, "canvas1")
 register(Canvas2, "canvas2")
 register(FPSMonitor, "js-fps-monitor")
