96996906f08bc1ada49b887cefb82cac68038e41
Yoshiya Hinosawa <stibium121@gmail.com>
2024-07-02 14:29:16 +0900

    chore: update build settings

--- src/main.ts
+++ src/main.ts
@@ -18,33 +18,44 @@ function loadImages(paths: string[]): Promise<HTMLImageElement[]> {
   return Promise.all(paths.map(loadImage))
 }
 
+class Brush {
+  constructor(public ctx: CanvasRenderingContext2D) {}
+
+  drawImage(img: HTMLImageElement, x: number, y: number, w: number, h: number) {
+    this.ctx.clearRect(x, y, w, h)
+    this.ctx.drawImage(img, x, y)
+  }
+}
+
 function Canvas1({ el, pub }: Context<HTMLCanvasElement>) {
-  const WIDTH = +el.width
-  const HEIGHT = +el.height
+  const WIDTH = el.width
+  const HEIGHT = el.height
 
   const W = Math.floor(WIDTH / 16)
   const H = Math.floor(HEIGHT / 16)
 
   const canvasCtx = el.getContext("2d")!
+  const brush = new Brush(canvasCtx)
 
   loadImages([
-    "./assets/char/juni/juni_b0.png",
-    "./assets/char/juni/juni_b1.png",
-    "./assets/char/juni/juni_f0.png",
-    "./assets/char/juni/juni_f1.png",
-    "./assets/char/juni/juni_l0.png",
-    "./assets/char/juni/juni_l1.png",
-    "./assets/char/juni/juni_r0.png",
-    "./assets/char/juni/juni_r1.png",
+    "./char/juni/juni_b0.png",
+    "./char/juni/juni_b1.png",
+    "./char/juni/juni_f0.png",
+    "./char/juni/juni_f1.png",
+    "./char/juni/juni_l0.png",
+    "./char/juni/juni_l1.png",
+    "./char/juni/juni_r0.png",
+    "./char/juni/juni_r1.png",
   ]).then((images) => {
     const loop = gameloop(() => {
       const i = Math.floor(Math.random() * W)
       const j = Math.floor(Math.random() * H)
-      canvasCtx.clearRect(i * 16, j * 16, 16, 16)
-      canvasCtx.drawImage(
+      brush.drawImage(
         images[Math.floor(Math.random() * 8)],
         i * 16,
         j * 16,
+        16,
+        16,
       )
     }, 60)
     loop.onStep((fps) => pub("fps", fps))
