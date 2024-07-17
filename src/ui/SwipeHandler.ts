e06fa3d08bedc1ac32e5a433e2d1988d23c9b8b1
Yoshiya Hinosawa <stibium121@gmail.com>
2024-07-18 00:21:48 +0900

    refactor: use constant

--- src/ui/SwipeHandler.ts
+++ src/ui/SwipeHandler.ts
@@ -3,17 +3,20 @@ import type { Context } from "@kt3k/cell"
 import { clearInput, Input } from "../util/dir.ts"
 import { getDir, getDistance } from "../util/touch.ts"
 
+const TOUCH_SENSITIVITY_THRESHOLD = 25
+
 export function SwipeHandler({ on }: Context) {
   let prevTouch: Touch | undefined
   on.touchstart = (e: TouchEvent) => {
     prevTouch = e.touches[0]
   }
+  // passive false is necessary to prevent scrolling in iOS Safari
   on({ passive: false }).touchmove = (e: TouchEvent) => {
     e.preventDefault()
     const touch = e.changedTouches[0]
     if (prevTouch) {
       const dist = getDistance(touch, prevTouch)
-      if (dist < 25) {
+      if (dist < TOUCH_SENSITIVITY_THRESHOLD) {
         return
       }
       clearInput()
