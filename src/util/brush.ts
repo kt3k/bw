937694435d6673a6419fa4d7c404d6d284b092f2
Yoshiya Hinosawa <stibium121@gmail.com>
2024-09-19 01:15:08 +0900

    refactor: rename to CanvasLayer

--- src/util/brush.ts
+++ src/util/brush.ts
@@ -1,5 +1,5 @@
 /** A simple wrapper of CanvasRenderingContext2D */
-export class DrawLayer {
+export class CanvasLater {
   #ctx: CanvasRenderingContext2D
   constructor(canvas: HTMLCanvasElement) {
     this.#ctx = canvas.getContext("2d")!
