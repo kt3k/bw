fceaca32043c07e5c9cdeb357e46a4ac6cdeb365
Yoshiya Hinosawa <stibium121@gmail.com>
2024-12-26 01:01:36 +0900

    fix slow types

--- src/util/math.ts
+++ src/util/math.ts
@@ -1,15 +1,15 @@
 /** Floors the given number to the multiple of the given n */
-export function floorN(x: number, n: number) {
+export function floorN(x: number, n: number): number {
   return Math.floor(x / n) * n
 }
 
 /** Ceils the given number to the multiple of the given n */
-export function ceilN(x: number, n: number) {
+export function ceilN(x: number, n: number): number {
   return Math.ceil(x / n) * n
 }
 
 /** Returns x modulo m */
-export function modulo(x: number, m: number) {
+export function modulo(x: number, m: number): number {
   const r = x % m
   return r >= 0 ? r : r + m
 }
