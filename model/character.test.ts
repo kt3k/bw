ebddeea6c4e0e47ad04d134865066af3eff94d41
Yoshiya Hinosawa <stibium121@gmail.com>
2025-07-25 10:59:06 +0900

    chore: update test

--- model/character.test.ts
+++ model/character.test.ts
@@ -3,10 +3,10 @@ import { assertEquals } from "@std/assert"
 
 Deno.test("Character", async (t) => {
   await t.step("physicalGridKey", () => {
-    const mc = new MainCharacter(100, 100, 1, "main/")
+    const mc = new MainCharacter(100, 100, "main/", "main", "down")
     assertEquals(mc.physicalGridKey, "100.100")
 
-    const npc = new RandomWalkNPC(200, 200, 1, "npc/")
+    const npc = new RandomWalkNPC(200, 200, "npc/", "npc", "left")
     assertEquals(npc.physicalGridKey, "200.200")
   })
 })
